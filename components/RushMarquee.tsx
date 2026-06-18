557453138bbc21a00672788e8fa17b6447704ee8
koahewlett <johnnykoa24@gmail.com>
2026-06-17 21:39:08 -0500

    Split marquee phrases and opposing entrances

--- components/RushMarquee.tsx
+++ components/RushMarquee.tsx
@@ -2,37 +2,19 @@
 
 import { motion, useReducedMotion, useScroll, useTransform } from 'framer-motion';
 
-const marqueePhrase = 'Exceptional leaders. Uncommon opportunities.';
-const topWords = [marqueePhrase, marqueePhrase, marqueePhrase];
-const bottomWords = [marqueePhrase, marqueePhrase, marqueePhrase];
-
-function MarqueeWords({ words }: { words: string[] }) {
-  const track = [...words, ...words];
-
-  return (
-    <>
-      {track.map((word, index) => (
-        <span key={`${word}-${index}`}>
-          {word}
-        </span>
-      ))}
-    </>
-  );
-}
-
 export function RushMarquee() {
   const reduceMotion = useReducedMotion();
   const { scrollYProgress } = useScroll();
-  const leftX = useTransform(scrollYProgress, [0, 0.28], ['-14%', '5%']);
-  const rightX = useTransform(scrollYProgress, [0, 0.28], ['14%', '-5%']);
+  const leftX = useTransform(scrollYProgress, [0, 0.28], ['-100vw', '5vw']);
+  const rightX = useTransform(scrollYProgress, [0, 0.28], ['100vw', '-5vw']);
 
   return (
     <div className="rush-marquee" aria-hidden="true">
       <motion.div className="rush-marquee-row" style={{ x: reduceMotion ? 0 : leftX }}>
-        <MarqueeWords words={topWords} />
+        <span>Exceptional leaders.</span>
       </motion.div>
       <motion.div className="rush-marquee-row rush-marquee-row-reverse" style={{ x: reduceMotion ? 0 : rightX }}>
-        <MarqueeWords words={bottomWords} />
+        <span>Uncommon opportunities.</span>
       </motion.div>
     </div>
   );
