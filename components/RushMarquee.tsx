e00213cbd596539d7b106dff5a31e8f63f014e13
koahewlett <johnnykoa24@gmail.com>
2026-06-26 23:59:29 -0500

    Tighten rush marquee scroll alignment

--- components/RushMarquee.tsx
+++ components/RushMarquee.tsx
@@ -5,17 +5,19 @@ import { motion, useReducedMotion, useScroll, useTransform } from 'framer-motion
 export function RushMarquee() {
   const reduceMotion = useReducedMotion();
   const { scrollYProgress } = useScroll();
-  const leftX = useTransform(scrollYProgress, [0, 0.28], ['-100vw', '5vw']);
-  const rightX = useTransform(scrollYProgress, [0, 0.28], ['100vw', '-5vw']);
+  const leftX = useTransform(scrollYProgress, [0, 0.12, 0.28], ['-105vw', '0vw', '4vw']);
+  const rightX = useTransform(scrollYProgress, [0, 0.12, 0.28], ['105vw', '0vw', '-4vw']);
 
   return (
     <div className="rush-marquee" aria-hidden="true">
-      <motion.div className="rush-marquee-row" style={{ x: reduceMotion ? 0 : leftX }}>
-        <span>Exceptional leaders.</span>
-      </motion.div>
-      <motion.div className="rush-marquee-row rush-marquee-row-reverse" style={{ x: reduceMotion ? 0 : rightX }}>
-        <span>Uncommon opportunities.</span>
-      </motion.div>
+      <div className="rush-marquee-inner">
+        <motion.div className="rush-marquee-row" style={{ x: reduceMotion ? 0 : leftX }}>
+          <span>Exceptional leaders.</span>
+        </motion.div>
+        <motion.div className="rush-marquee-row rush-marquee-row-reverse" style={{ x: reduceMotion ? 0 : rightX }}>
+          <span>Uncommon opportunities.</span>
+        </motion.div>
+      </div>
     </div>
   );
 }
