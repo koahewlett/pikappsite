32e4df44069b6e823aae8211d449fc4683cf23fa
koahewlett <johnnykoa24@gmail.com>
2026-06-26 23:59:11 -0500

    Use single SVG hero shield

--- components/HeroCrests.tsx
+++ components/HeroCrests.tsx
@@ -1,12 +1,17 @@
+import Image from 'next/image';
+
 export function HeroCrests() {
   return (
     <div className="hero-crests" aria-hidden="true">
-      <div className="hero-crest hero-crest-left">
-        <div className="chrome-crest" />
-      </div>
-
-      <div className="hero-crest hero-crest-right">
-        <div className="chrome-crest" />
+      <div className="hero-crest hero-crest-right hero-svg-crest">
+        <Image
+          src="/images/pi-kapp-hero-shield.svg"
+          alt=""
+          fill
+          priority
+          sizes="(min-width: 1024px) 52rem, 88vw"
+          className="pi-kapp-hero-shield"
+        />
       </div>
     </div>
   );
